b6dc8225de733d48e1db3e365fd1fba026254c87
arjin <arjin@ITINNOH-DW0193.ikea.com>
2017-09-14 10:01:11 +0200

    Just addin thanks line

--- HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
+++ HelloWorldJavaEE/src/net/codejava/javaee/HelloServlet.java
@@ -32,6 +32,7 @@ public class HelloServlet extends HttpServlet {
 		String yourName = request.getParameter("yourName");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.println("<h1>Thanks " + yourName + "</h1>");
 		writer.close();
 	}
 
